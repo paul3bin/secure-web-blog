2a4016acd64f964d459e036f266f773030d0f1e8
Ebin Paul <ebinpaul@outlook.com>
2023-04-26 12:36:43 +0100

    updated email service to send emails through sendgrid

--- services/email.service.js
+++ services/email.service.js
@@ -1,39 +1,25 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
-// async..await is not allowed in global scope, must use a wrapper
-async function sendEmail(recipient, subject, text) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+async function sendEmail(to, subject, text) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.SENDER_EMAIL_ID, // generated ethereal user
-      pass: process.env.SENDER_EMAIL_PW, // generated ethereal password
-    },
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Blog Authentication" <blog.authentication@example.com>', // sender address
-    to: `${recipient}`, // list of receivers
-    subject: `${subject}`, // Subject line
-    text: `${text}`, // plain text body
-    html: `<b>${text}</b>`, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  const msg = {
+    to: to, // Change to your recipient
+    from: process.env.SENDGRID_VERIFIED_SENDER, // Change to your verified sender
+    subject: subject,
+    text: text,
+    html: `<strong>${text}</strong>`,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 module.exports = { sendEmail };
