cdc71cb346614bfc0cc8577bfa5429ff955894ea
Ebin Paul <ebinpaul@outlook.com>
2023-05-13 17:58:42 +0100

    changes for setting csrf token to cookies

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -7,16 +7,16 @@ const apiEndpoint = apiUrl + "/auth";
 const tokenKey = "token";
 //const cookies = new Cookies();
 export async function login(email, password) {
-  const response = await http.post("/user/v1/signIn", { email, password });    
-  if(response && response.data && response.data.status=='pass'){   
-    
-        cookies.set('token', response.data.token, { path: '/'});
-        cookies.set('otp', response.data.otp, { path: '/'});
-        // cookies.set('token', response.data.token, 
-        //   { path: '/',
-        //   maxAge: 3600000, // 1 hour
-        //   secure: false, // set to true if you're using https
-        //   httpOnly: true });
+  const response = await http.post("/user/v1/signIn", { email, password });
+  if (response && response.data && response.data.status == "pass") {
+    cookies.set("token", response.data.token, { path: "/" });
+    cookies.set("otp", response.data.otp, { path: "/" });
+    cookies.set("csrf", response.headers["x-csrf-token"], { path: "/" });
+
+    //   { path: '/',
+    //   maxAge: 3600000, // 1 hour
+    //   secure: false, // set to true if you're using https
+    //   httpOnly: true });
     http.setJwt(getJwt());
   }
   return response;
@@ -28,12 +28,12 @@ export function loginWithJwt(jwt) {
 
 export async function logout() {
   await http.post("/user/v1/signout");
-  cookies.remove('token');
+  cookies.remove("token");
 }
 
 export function getCurrentUser() {
   try {
-    const jwt = cookies.get('token');
+    const jwt = cookies.get("token");
     return jwtDecode(jwt);
   } catch (ex) {
     return null;
@@ -41,12 +41,12 @@ export function getCurrentUser() {
 }
 
 export function getJwt() {
-  const jwt = cookies.get('token');
+  const jwt = cookies.get("token");
   return jwt;
 }
 
 export async function verifyLoginOtp(otp) {
-  const response = await http.post("/user/v1/verify", { "otp" : otp });  
+  const response = await http.post("/user/v1/verify", { otp: otp });
   return response;
 }
 
@@ -56,5 +56,5 @@ export default {
   logout,
   getCurrentUser,
   getJwt,
-  verifyLoginOtp
+  verifyLoginOtp,
 };
