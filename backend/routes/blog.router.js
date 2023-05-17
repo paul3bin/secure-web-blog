ded91c82b08b9f495626404445d8869e9d142a80
Bhasker Rao <mail2bachi@gmail.com>
2023-05-17 11:31:38 +0100

    Final

--- backend/routes/blog.router.js
+++ backend/routes/blog.router.js
@@ -5,35 +5,39 @@ const util = require("../utils/auth");
 
 router.post(
   "/v1/",
-  util.RateLimit(),
+  /* util.RateLimit(),*/
   util.authorize(),
   util.verifyCSRF(),
   blogController.create
 );
 router.post(
   "/search/v1",
-  util.RateLimit(),
+  /* util.RateLimit(),*/
   util.allow(),
   blogController.search
 );
-router.get("/v1/", util.RateLimit(), util.allow(), blogController.get);
-router.get("/v1/:id", util.RateLimit(), util.allow(), blogController.getById);
+router.get("/v1/", /*util.RateLimit(),*/ util.allow(), blogController.get);
+router.get(
+  "/v1/:id",
+  /*util.RateLimit(),*/ util.allow(),
+  blogController.getById
+);
 router.get(
   "/user/v1",
-  util.RateLimit(),
+  /*util.RateLimit(),*/
   util.authorize(),
   blogController.getByUser
 );
 router.delete(
   "/v1/:id",
-  util.RateLimit(),
+  /*util.RateLimit(),*/
   util.authorize(),
   util.verifyCSRF(),
   blogController.remove
 );
 router.put(
   "/v1/:id",
-  util.RateLimit(),
+  /*util.RateLimit(),*/
   util.authorize(),
   util.verifyCSRF(),
   blogController.update
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 const auth = require("../utils/auth");
 
-router.post("/v1/signup", auth.RateLimit(), userController.create);
+router.post("/v1/signup", /*auth.RateLimit(),*/ userController.create);
 router.get(
   "/v1/:id",
-  auth.RateLimit(),
+  /*auth.RateLimit(),*/
   auth.authorize(),
   userController.getById
 );
-router.post("/v1/signin", auth.RateLimit(), userController.signIn);
+router.post("/v1/signin", /*auth.RateLimit(),*/ userController.signIn);
 router.post(
   "/v1/verify",
-  auth.RateLimit(),
+  /*auth.RateLimit(),*/
   auth.verifyCSRF(),
   userController.verify
 );
-router.post("/v1/signout", auth.RateLimit(), userController.signOut);
+router.post("/v1/signout", /* auth.RateLimit(),*/ userController.signOut);
 router.post(
   "/v1/verifyRegistration",
-  auth.RateLimit(),
+  /* auth.RateLimit(),*/
   userController.verifyRegistration
 );
 
