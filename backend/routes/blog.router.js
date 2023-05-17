8a8205074054a125d20b31ba2a3dfa27dcd352ed
Bhasker Rao <mail2bachi@gmail.com>
2023-05-17 09:23:12 +0100

    final fixes

--- backend/routes/blog.router.js
+++ backend/routes/blog.router.js
@@ -3,19 +3,37 @@ const router = express.Router();
 const blogController = require("../controllers/blog.controller");
 const util = require("../utils/auth");
 
-router.post("/v1/", util.authorize(), util.verifyCSRF(), blogController.create);
-router.post("/search/v1", util.allow(), blogController.search);
-router.get("/v1/", util.allow(), blogController.get);
-router.get("/v1/:id", util.allow(), blogController.getById);
-router.get("/user/v1", util.authorize(), blogController.getByUser);
+router.post(
+  "/v1/",
+  util.RateLimit(),
+  util.authorize(),
+  util.verifyCSRF(),
+  blogController.create
+);
+router.post(
+  "/search/v1",
+  util.RateLimit(),
+  util.allow(),
+  blogController.search
+);
+router.get("/v1/", util.RateLimit(), util.allow(), blogController.get);
+router.get("/v1/:id", util.RateLimit(), util.allow(), blogController.getById);
+router.get(
+  "/user/v1",
+  util.RateLimit(),
+  util.authorize(),
+  blogController.getByUser
+);
 router.delete(
   "/v1/:id",
+  util.RateLimit(),
   util.authorize(),
   util.verifyCSRF(),
   blogController.remove
 );
 router.put(
   "/v1/:id",
+  util.RateLimit(),
   util.authorize(),
   util.verifyCSRF(),
   blogController.update
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -3,11 +3,25 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 const auth = require("../utils/auth");
 
-router.post("/v1/signup", userController.create);
-router.get("/v1/:id", auth.authorize(), userController.getById);
-router.post("/v1/signin", userController.signIn);
-router.post("/v1/verify", auth.verifyCSRF(), userController.verify);
-router.post("/v1/signout", userController.signOut);
-router.post("/v1/verifyRegistration", userController.verifyRegistration);
+router.post("/v1/signup", auth.RateLimit(), userController.create);
+router.get(
+  "/v1/:id",
+  auth.RateLimit(),
+  auth.authorize(),
+  userController.getById
+);
+router.post("/v1/signin", auth.RateLimit(), userController.signIn);
+router.post(
+  "/v1/verify",
+  auth.RateLimit(),
+  auth.verifyCSRF(),
+  userController.verify
+);
+router.post("/v1/signout", auth.RateLimit(), userController.signOut);
+router.post(
+  "/v1/verifyRegistration",
+  auth.RateLimit(),
+  userController.verifyRegistration
+);
 
 module.exports = router;
